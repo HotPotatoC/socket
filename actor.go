f5d6578f2ba21abb8d92c46e8e727ca68154381a
Entalizen <danielarya185@gmail.com>
2020-10-29 23:58:38 +0700

    Add more explanation and conn field

--- actor.go
+++ actor.go
@@ -1,8 +1,12 @@
 package ws
 
+import "net"
+
 // Actor struct containing information of actor
+// Actor can be said as an connected client
 type Actor struct {
-	id *string
+	id   *string
+	conn *net.Conn
 }
 
 // ID return id information
