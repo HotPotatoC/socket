37490977968edbe38c284010cdaf83905c3507c2
Entalizen <danielarya185@gmail.com>
2020-10-30 02:02:42 +0700

    Add UIDLength field

--- config.go
+++ config.go
@@ -6,10 +6,12 @@ import "time"
 type Config struct {
 	Timeout       time.Duration
 	HostWhitelist []string
+	UIDLength     int
 }
 
 // DefaultConfig setting up for default connection config
 var DefaultConfig = Config{
 	Timeout:       time.Second * 4,
 	HostWhitelist: make([]string, 0),
+	UIDLength:     16,
 }
