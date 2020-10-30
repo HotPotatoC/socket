12a67104e8052b85941947aeb4e4e793d89e04d1
Entalizen <danielarya185@gmail.com>
2020-10-30 22:02:25 +0700

    Impr conn_test.go

--- conn_test.go
+++ conn_test.go
@@ -10,7 +10,9 @@ import (
 func TestSocket_Listen(t *testing.T) {
 	ws := socket.CreateWebSocket()
 	ws.Callback(func(c *socket.Context) error {
-		fmt.Println(c.Event().Type(), string(c.Message().Bytes()))
+		if ok, err := c.Event().Type().Eq(socket.TypeConnected); err == nil && ok {
+			fmt.Printf("Someone connected with id: '%v'", c.Sender().ID())
+		}
 		return nil
 	})
 	ws.Listen(8080)
