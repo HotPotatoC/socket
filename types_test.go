10f06470ca904c7ccceb9c766a54194d12b800e4
Entalizen <danielarya185@gmail.com>
2020-10-30 22:17:42 +0700

    Test add more test for typecode eq

--- types_test.go
+++ types_test.go
@@ -12,4 +12,28 @@ func TestTypeCode_Eq(t *testing.T) {
 	if x, y := etype.Eq(ws.OpText); y != nil || !x {
 		t.Error("Why this is wrong?, this shoud be true")
 	}
+
+	if x, y := etype.Eq(socket.TypeContinuation); y != nil || x {
+		t.Error("Why this is wrong?, this shoud be false")
+	}
+
+	if x, y := etype.Eq(socket.TypeConnected); y != nil || x {
+		t.Error("Why this is wrong?, this shoud be false")
+	}
+
+	if x, y := etype.Eq(socket.TypeClose); y != nil || x {
+		t.Error("Why this is wrong?, this shoud be false")
+	}
+
+	if x, y := etype.Eq(socket.TypePing); y != nil || x {
+		t.Error("Why this is wrong?, this shoud be false")
+	}
+
+	if x, y := etype.Eq(socket.TypePong); y != nil || x {
+		t.Error("Why this is wrong?, this shoud be false")
+	}
+
+	if x, y := etype.Eq(socket.TypeBinary); y != nil || x {
+		t.Error("Why this is wrong?, this shoud be false")
+	}
 }
