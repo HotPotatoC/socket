086ad9f307d41e67621997a3df0c462beac16e89
Entalizen <danielarya185@gmail.com>
2020-10-30 18:30:18 +0700

    Test to listen a connection

---
+++ conn_test.go
@@ -0,0 +1,18 @@
+package socket_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Stalync/socket"
+)
+
+func TestSocket_Listen(t *testing.T) {
+
+	ws := socket.CreateWebSocket()
+	ws.Callback(func(c *socket.Context) error {
+		fmt.Println(c.Event().Type(), string(*c.Message().data))
+		return nil
+	})
+	ws.Listen(8080)
+}
