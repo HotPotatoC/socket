dfb43b08cdd47ff60303e9c397baba7b4eae2b58
Entalizen <danielarya185@gmail.com>
2020-10-30 18:28:26 +0700

    Fix context builder waited twice

--- io.go
+++ io.go
@@ -4,24 +4,32 @@ import (
 	"io"
 
 	"github.com/gobwas/ws"
-	"github.com/gobwas/ws/wsutil"
 )
 
 func (s *Socket) contextBuilder(a *Actor) (*Context, error) {
-	header, err := ws.ReadHeader(*a.conn)
+	h, err := ws.ReadHeader(*a.conn)
 	if err != nil {
-
+		return nil, err
 	}
-	payload := make([]byte, header.Length)
+
+	// payload := make([]byte, 1024)
+	payload := make([]byte, h.Length)
+	// reader := io.LimitReader(*a.conn, h.Length)
+	// _, err = reader.Read(payload)
 	_, err = io.ReadFull(*a.conn, payload)
 	if err != nil {
-		// handle error
+		return nil, err
 	}
-	data, _, err := wsutil.ReadClientData(*a.conn)
-
+	if h.Masked {
+		ws.Cipher(payload, h.Mask, 0)
+	}
 	ctx := createContext(s.config)
-	ctx.event.header = &header
-	ctx.message.data = &data
+	ctx.event = Event{
+		header: &h,
+	}
+	ctx.message = Message{
+		data: &payload,
+	}
 	ctx.sender = *a
 	return ctx, nil
 }
