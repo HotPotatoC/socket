0b7c2d8ff0547261634951fdbf3d05576bdecd0c
Entalizen <danielarya185@gmail.com>
2020-10-30 23:20:08 +0700

    Fix sorry i forget to put message data in context builder

--- io.go
+++ io.go
@@ -31,7 +31,7 @@ func (s *Socket) contextBuilder(a *Actor) (*Context, error) {
 	}
 
 	ctx := createContext(s.config)
-
+	ctx.message.data = &payload
 	ctx.event.h = &h
 	ctx.event.code = &code
 	ctx.sender = a
