a75ec6c5bd063bd791b00577e04307e2a3a67e66
Entalizen <danielarya185@gmail.com>
2020-11-01 01:36:17 +0700

    Test improve testing

--- conn_test.go
+++ conn_test.go
@@ -12,11 +12,24 @@ func TestSocket_Listen(t *testing.T) {
 
 	server.Callback(func(c *socket.Context) (err error) {
 
-		if ok, _ := c.Event().Type().Eq(socket.TypeText); ok {
-			return c.Sender().SendText(c.Message().String())
+		switch c.Event().Type() {
+		case socket.TypeConnected:
+			fmt.Println(fmt.Sprintf("Master %v is connected", c.Sender().ID()))
+			err = c.Sender().SendText("Hellow Master")
+			break
+		case socket.TypeText:
+			message := c.Message().String()
+			if message == "exit" {
+				return server.CloseNormalClosure(c.Sender(), "Byeeee Master")
+			}
+			err = c.Sender().SendText(message)
+			break
+		case socket.TypeDisconnected:
+			fmt.Println(fmt.Sprintf("Master %v is disconnected", c.Sender().ID()))
+			break
 		}
 
-		return nil
+		return
 	})
 
 	fmt.Println(server.Listen(8000))
