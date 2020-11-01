bf4e7d0c1ca45ead5f6d413413366052df7c89c3
Entalizen <danielarya185@gmail.com>
2020-11-01 13:09:50 +0700

    Add PING and PONG function to check client

--- actor.go
+++ actor.go
@@ -4,6 +4,13 @@ import (
 	"net"
 )
 
+// this variable used to check actor connection
+// wheter still connected or not
+var (
+	PING = []byte("PING")
+	PONG = []byte("PING")
+)
+
 // Actor struct containing information of actor
 // Actor can be said as an connected client
 type Actor struct {
@@ -25,4 +32,9 @@ func (actor *Actor) SendText(message string) error {
 // SendBytes write byte of data to this actor
 func (actor *Actor) SendBytes(data []byte) error {
 	return frameCompiler(actor, TypeBinary, data)
+}
+
+// PING function to send Ping message
+func (actor *Actor) PING() error {
+	return frameCompiler(actor, TypePing, PING)
 }
